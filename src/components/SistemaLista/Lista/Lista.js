b9f2a7c0f694c9e4a46107e0a3a1ec9d2ee95d01
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-08-05 17:30:49 -0300

    Adicionar campo para criação de itens da Lista

--- src/components/SistemaLista/Lista/Lista.js
+++ src/components/SistemaLista/Lista/Lista.js
@@ -6,17 +6,10 @@ class Lista extends Component {
     render() {
         return (
             <section className="Lista">
-                <div className="item">
-                    <p>TESTE</p>
-                    <button>X</button>
-                </div>
-                <div className="item">
-                    <p>TESTE</p>
-                    <button>X</button>
-                </div>
-                <div className="item">
-                    <p>TESTE</p>
-                    <button>X</button>
+                <div className="criacao-item">
+                    <input type="text" name="" />
+                    <button>Criar</button>
+                    <button>Sortear</button>
                 </div>
                 <div className="item">
                     <p>TESTE</p>
